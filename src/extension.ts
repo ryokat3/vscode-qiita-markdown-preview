cd461894cf7f321e85e14d404f21a2e5be736312
ryokat3 <ryokat3@gmail.com>
2022-02-03 12:29:13 +0900

    Added configuration

--- src/extension.ts
+++ src/extension.ts
@@ -7,24 +7,22 @@ import { markdownItQiitaSyncHeader } from "./qiita-sync-header"
 import { markdownItQiitaCode } from "./qiita-code"
 import { markdownItQiitaStyle } from "./qiita-style"
 import { markdownItQiitaLinkCard } from "./qiita-link-card"
-import * as os from "os"
-import * as path from "path"
-import * as fs from "fs"
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const configSection = 'qiita-markdown-preview';
+const enableKey = 'enable';
+
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-qiita-markdown-preview" is now active!')
 
-	return {
+	return vscode.workspace.getConfiguration(configSection, null).get(enableKey) ? {
 		extendMarkdownIt(md: MarkdownIt) {			
 			return md.use(markdownItQiitaStyle)
 				.use(markdownItQiitaNote).use(markdownItQiitaSyncHeader)
 				.use(markdownItQiitaCode).use(markdownItQiitaLinkCard)
 		}
+	} : {
+		extendMarkdownIt(md: MarkdownIt) {
+			return md
+		}		
 	}
 }
 
